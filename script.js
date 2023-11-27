07a02ff8ba2a10d08d3e5b84f7d583069b0dd574
Tatien Miller <tatmil99@gmail.com>
2023-11-27 03:53:57 -0600

    toggles form and applies styles based on open/close state

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const newBtn = document.querySelector(".new-btn");
 const closeBtn = document.querySelector(".close-btn");
-
 newBtn.addEventListener("click", handleForm);
 closeBtn.addEventListener("click", handleForm);
 
 function handleForm(e) {
   const form = document.querySelector(".form-container");
-  const background = document.querySelectorAll("header, main, footer");
+  const container = document.querySelector(".container");
+  const btnClass = e.target.className;
 
-  if (e.target.className == "new-btn") {
+  if (btnClass == "new-btn") {
     form.style.display = "flex";
-    background.style.filter = "blur(4px) brightness(40%)";
-  } else if (e.target.className == "close-btn") {
+    container.style.filter = "blur(4px) brightness(40%)";
+  } else if (btnClass == "close-btn") {
     form.style.display = "none";
-    background.style.filter = "none";
+    container.style.filter = "none";
   }
 }
