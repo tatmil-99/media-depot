3d60270dfbad94cee7f92ca1836000be147eec05
Tatien Miller <tatmil99@gmail.com>
2023-11-26 22:12:55 -0600

    creates nodeList to apply filter to background when form is opened

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ closeBtn.addEventListener("click", handleForm);
 
 function handleForm(e) {
   const form = document.querySelector(".form-container");
+  const background = document.querySelectorAll("header, main, footer");
 
   if (e.target.className == "new-btn") {
     form.style.display = "flex";
+    background.style.filter = "blur(4px) brightness(40%)";
   } else if (e.target.className == "close-btn") {
     form.style.display = "none";
+    background.style.filter = "none";
   }
 }
